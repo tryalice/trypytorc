6dc3610ea6d39a7102b13c49e55c1aecc7595745
tryalice <48514380+tryalice@users.noreply.github.com>
2020-04-15 05:55:44 +0200

    Initialize gh/suo/21/orig

---
+++ torch/csrc/api/src/jit.cpp
@@ -0,0 +1,20 @@
+#include <torch/jit.h>
+
+#include <torch/csrc/jit/script/compiler.h>
+#include <ATen/core/stack.h>
+
+#include <memory>
+#include <string>
+
+namespace torch {
+namespace jit {
+
+std::shared_ptr<script::CompilationUnit> compile(const std::string& source) {
+  auto module = std::make_shared<script::CompilationUnit>();
+  auto resolver = std::make_shared<script::NativeResolver>();
+  module->define(source, std::move(resolver), nullptr);
+  return module;
+}
+
+} // namespace jit
+} // namespace torch
