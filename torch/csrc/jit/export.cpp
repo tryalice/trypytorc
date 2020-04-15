3c6fdd719f968419a18189ffaedbe3ed15a61dd5
tryalice <48514380+tryalice@users.noreply.github.com>
2020-04-15 04:00:51 +0200

    Initialize win_py2.7

---
+++ torch/csrc/jit/export.cpp
@@ -0,0 +1,350 @@
+#include <Python.h>
+
+#include "torch/csrc/jit/export.h"
+#include "torch/csrc/onnx/onnx.h"
+#include "torch/csrc/autograd/symbolic.h"
+#include "torch/csrc/utils/python_numbers.h"
+#include "torch/csrc/utils/python_strings.h"
+#include "torch/csrc/Exceptions.h"
+
+#include "torch/csrc/utils/functional.h"
+#include <ATen/ATen.h>
+#include <ATen/optional.h>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <cstring>
+#include <fstream>
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace py = pybind11;
+
+namespace torch { namespace jit {
+
+namespace {
+
+namespace onnx = ::torch::onnx;
+
+std::string value_name(Value* n) {
+  return n->uniqueName();
+}
+
+void encodeGraph(onnx::GraphProto * p_g, const std::shared_ptr<Graph> & g,
+                 const std::vector<at::Tensor> & initializers,
+                 RawDataExportMap* raw_data_export_map=nullptr);
+
+void encodeTensor(onnx::TensorProto * p, const at::Tensor & tensor,
+                  at::optional<std::string> external_ref={},
+                  RawDataExportMap* raw_data_export_map = nullptr) {
+  for(auto d : tensor.sizes()) {
+    p->add_dims(d);
+  }
+  onnx::DataType onnx_type;
+  switch(tensor.type().scalarType()) {
+    case at::kDouble:
+      onnx_type = onnx::kDOUBLE;
+      break;
+    case at::kFloat:
+      onnx_type = onnx::kFLOAT;
+      break;
+    case at::kHalf:
+      onnx_type = onnx::kFLOAT16;
+      break;
+    case at::kByte:
+    case at::kChar:
+      onnx_type = onnx::kINT8;
+      break;
+    case at::kShort:
+      onnx_type = onnx::kINT16;
+      break;
+    case at::kInt:
+      onnx_type = onnx::kINT32;
+      break;
+    case at::kLong:
+      onnx_type = onnx::kINT64;
+      break;
+    default:
+      torch::barf("unexpected tensor scalar type");
+      break;
+  }
+  p->set_data_type(onnx_type);
+  // CPU's HalfTensor doesn't have contiguous(), so first calling contiguous()
+  auto t = tensor.contiguous().toBackend(at::kCPU);
+  // Add a buffer to the raw_data_export_map for the caller to dump into an
+  // external data store. If external_ref is not specified, we instead dump
+  // the contiguous data into the protobuf itself
+  if (external_ref) {
+    // For now, we use the name of the tensor as the external lookup name to
+    // avoid ONNX protobuf changes.
+    JIT_ASSERT(external_ref.value() == p->get_name());
+    JIT_ASSERT(raw_data_export_map != nullptr);
+    JIT_ASSERT(raw_data_export_map->count(external_ref.value()) == 0);
+    (*raw_data_export_map)[external_ref.value()] = t;
+    p->set_external_data_present();
+  } else {
+    p->set_raw_data(t);
+  }
+}
+
+void addAttribute(onnx::NodeProto * n_p, jit::Node * n, jit::Symbol name) {
+  auto attr = n_p->add_attribute();
+  JIT_ASSERT(name.is_attr());
+  attr->set_name(name.toUnqualString());
+  switch(n->kindOf(name)) {
+    case AttributeKind::f:
+      attr->set_f(n->f(name));
+      attr->set_type(onnx::aFLOAT);
+      break;
+    case AttributeKind::fs:
+      attr->set_type(onnx::aFLOATS);
+      for(auto & v : n->fs(name))
+        attr->add_floats(v);
+      break;
+    case AttributeKind::i:
+      attr->set_type(onnx::aINT);
+      attr->set_i(n->i(name));
+      break;
+    case AttributeKind::is:
+      attr->set_type(onnx::aINTS);
+      for(auto & v : n->is(name))
+        attr->add_ints(v);
+      break;
+    case AttributeKind::s:
+      attr->set_type(onnx::aSTRING);
+      attr->set_s(n->s(name));
+      break;
+    case AttributeKind::ss:
+      attr->set_type(onnx::aSTRINGS);
+      for(auto & v : n->ss(name))
+        attr->add_strings(v);
+      break;
+    case AttributeKind::t: {
+      attr->set_type(onnx::aTENSOR);
+      auto t = attr->mutable_t();
+      encodeTensor(t, n->t(name));
+    } break;
+    case AttributeKind::ts:
+      attr->set_type(onnx::aTENSORS);
+      for(auto & v : n->ts(name)) {
+        auto t = attr->add_tensors();
+        encodeTensor(t, v);
+      }
+      break;
+    case AttributeKind::g: {
+      attr->set_type(onnx::aGRAPH);
+      auto g = attr->mutable_g();
+      encodeGraph(g, n->g(name), {});
+    } break;
+    case AttributeKind::gs:
+      attr->set_type(onnx::aGRAPHS);
+      for(auto & v : n->gs(name)) {
+        auto g = attr->add_graphs();
+        encodeGraph(g, v, {});
+      }
+      break;
+  }
+}
+
+void encodeTypeProtoTensorType(onnx::TypeProtoTensor* tensor_type, Value* n) {
+  onnx::TensorShapeProto* shape = tensor_type->mutable_shape();
+  TensorType* node_type = n->type()->expect<TensorType>();
+  const std::vector<std::int64_t>& sizes = node_type->sizes();
+  for (std::int64_t s : sizes) {
+    shape->add_dim(s);
+  }
+  onnx::DataType onnx_type;
+  switch(node_type->scalarType()) {
+    case at::kDouble:
+      onnx_type = onnx::kDOUBLE;
+      break;
+    case at::kFloat:
+      onnx_type = onnx::kFLOAT;
+      break;
+    case at::kHalf:
+      onnx_type = onnx::kFLOAT16;
+      break;
+    case at::kByte:
+    case at::kChar:
+      onnx_type = onnx::kINT8;
+      break;
+    case at::kShort:
+      onnx_type = onnx::kINT16;
+      break;
+    case at::kInt:
+      onnx_type = onnx::kINT32;
+      break;
+    case at::kLong:
+      onnx_type = onnx::kINT64;
+      break;
+    default:
+      torch::barf("unexpected tensor scalar type");
+      break;
+  }
+  tensor_type->set_data_type(onnx_type);
+}
+
+void encodeValueInfo(onnx::ValueInfoProto* v, Value* n) {
+  v->set_name(value_name(n));
+  onnx::TypeProto* t = v->mutable_type();
+  onnx::TypeProtoTensor* tensor_type = t->mutable_tensor_type();
+  encodeTypeProtoTensorType(tensor_type, n);
+}
+
+void encodeGraph(onnx::GraphProto * p_g, const std::shared_ptr<Graph> & g,
+                 const std::vector<at::Tensor> & initializers,
+                 RawDataExportMap* raw_data_export_map) {
+  JIT_ASSERT(p_g != nullptr);
+  p_g->set_name("torch-jit-export");
+
+  for (auto input : g->inputs()) {
+    onnx::ValueInfoProto* v = p_g->add_input();
+    encodeValueInfo(v, input);
+  }
+  for (auto output : g->outputs()) {
+    onnx::ValueInfoProto* v = p_g->add_output();
+    encodeValueInfo(v, output);
+  }
+  for (auto node : g->nodes()) {
+    if (node->kind() == prim::Undefined) {
+      // Undefined nodes are used to implement optional inputs. One
+      // way to "not provide" an optional input is to create an
+      // Undefined node, and pass its output as that input.
+      continue;
+    }
+    auto p_n = p_g->add_node();
+    if (node->getSourceLocation()) {
+      std::stringstream ss;
+      node->getSourceLocation()->highlight(ss);
+      p_n->set_doc_string(ss.str());
+    }
+    for(auto input : node->inputs()) {
+      if (input->node()->kind() == prim::Undefined) {
+        p_n->add_input("");
+      } else {
+        p_n->add_input(value_name(input));
+      }
+    }
+    for(auto output : node->outputs()) {
+      p_n->add_output(value_name(output));
+    }
+    JIT_ASSERT(node->kind().is_onnx());
+    p_n->set_op_type(node->kind().toUnqualString());
+    for(auto attr_name : node->attributeNames()) {
+      addAttribute(p_n, node, attr_name);
+    }
+  }
+  auto num_initializers = initializers.size();
+  int inputs_count = g->inputs().size() - num_initializers;
+  for (auto & tensor : initializers) {
+    // TODO: stop using positions to determine which initializers
+    // match to which inputs
+    std::string name = p_g->get_input_name(inputs_count++);
+    auto p = p_g->add_initializer();
+    p->set_name(name);
+    if (raw_data_export_map) {
+      encodeTensor(p, tensor, name, raw_data_export_map);
+    } else {
+      encodeTensor(p, tensor, {});
+    }
+  }
+}
+
+void encodeModel(onnx::ModelProto* p_m, const std::shared_ptr<Graph>& g,
+                 const std::vector<at::Tensor>& initializers,
+                 RawDataExportMap* raw_data_export_map = nullptr) {
+  onnx::GraphProto* p_g = p_m->mutable_graph();
+  encodeGraph(p_g, g, initializers, raw_data_export_map);
+}
+
+namespace {
+std::string getNodeStackTraceString(Node* n) {
+  std::stringstream ss;
+  if (n->getSourceLocation()) {
+    n->getSourceLocation()->highlight(ss);
+  } else {
+    ss << "<unknown location>";
+  }
+  return ss.str();
+}
+} // namespace
+
+void validateGraph(const std::shared_ptr<Graph>& graph) {
+  for (auto node : graph->nodes()) {
+      // Macro'ed so we get a marginally better line number on failed export
+#define FAIL_EXPORT(name) \
+      throw std::runtime_error(std::string("ONNX export failed: ") + name + "\n\nGraph we tried to export:\n" + graph->toString());
+    IR_IF(node, CppOp)
+      auto cpp_node = static_cast<torch::jit::CppOp*>(value);
+      FAIL_EXPORT(
+          "Couldn't export C++ operator " + cpp_node->name() +
+          "\n\nDefined at:\n" + getNodeStackTraceString(node))
+      IR_ELSEIF(PythonOp)
+      auto py_node = static_cast<torch::jit::PythonOp*>(value);
+      FAIL_EXPORT(
+          "Couldn't export Python operator " + py_node->name() +
+          "\n\nDefined at:\n" + getNodeStackTraceString(node))
+      IR_ELSE()
+      // Special error messages for certain types of operators
+      if (node->kind() == aten::expand) {
+        FAIL_EXPORT(
+            "Could not export a broadcasted operation; ONNX likely does not support this form of broadcasting.\n\nBroadcast occurred at:\n" +
+            getNodeStackTraceString(node));
+      }
+      if (node->kind() == prim::PackPadded || node->kind() == prim::PadPacked) {
+        FAIL_EXPORT(
+            "Cannot export individual pack_padded_sequence or pad_packed_sequence; these operations must occur in pairs.\n\nUsage of this operation occurred at:\n" +
+            getNodeStackTraceString(node));
+      }
+      if (!node->kind().is_onnx() && node->kind() != prim::Undefined) {
+        FAIL_EXPORT(
+            "Couldn't export operator " + node->kind().toDisplayString() + "\n\nDefined at:\n" +
+            getNodeStackTraceString(node));
+      }
+    IR_END()
+#undef FAIL_EXPORT
+  }
+}
+
+}
+
+std::tuple<std::string, RawDataExportMap> ExportGraph(
+                        const std::shared_ptr<Graph>& graph,
+                        const std::vector<at::Tensor> & initializers,
+                        int64_t onnx_opset_version,
+                        bool defer_weight_export) {
+
+  validateGraph(graph);
+
+  ::torch::onnx::ModelProto model_proto;
+  model_proto.set_producer_name("pytorch");
+  model_proto.set_producer_version("0.3");
+  auto* imp = model_proto.add_opset_import();
+  // This is the version of ONNX operator set we are targeting
+  imp->set_version(onnx_opset_version);
+
+  // Map {external_data_ref -> raw data} for external serialization of weights
+  RawDataExportMap raw_data_export_map;
+
+  // Set up nanopb callbacks and compute the amount of space needed to store
+  // the resulting protobuf
+  if (defer_weight_export) {
+    encodeModel(&model_proto, graph, initializers, &raw_data_export_map);
+  } else {
+    encodeModel(&model_proto, graph, initializers);
+  }
+
+  size_t out_size;
+  pb_get_encoded_size(&out_size, onnx_ModelProto_fields, &model_proto.proto);
+
+  // Allocate storage and export the graph
+  std::string out(out_size, '\0');
+  pb_ostream_t ostream = pb_ostream_from_buffer(reinterpret_cast<pb_byte_t *>(&out[0]), out_size);
+  pb_encode(&ostream, onnx_ModelProto_fields, &model_proto.proto);
+
+  return std::make_tuple(out, raw_data_export_map);
+}
+
+}}
---
+++ torch/nn/modules/instancenorm.py
@@ -0,0 +1,228 @@
+from .batchnorm import _BatchNorm
+from .. import functional as F
+
+
+class _InstanceNorm(_BatchNorm):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=False,
+                 track_running_stats=False):
+        super(_InstanceNorm, self).__init__(
+            num_features, eps, momentum, affine, track_running_stats)
+
+    def _check_input_dim(self, input):
+        return NotImplemented
+
+    def _load_state_dict_key_mismatch(self, full_name, name, is_missing):
+        if not is_missing and not self.track_running_stats and \
+                name in ('running_mean', 'running_var'):
+            raise KeyError(
+                'Unexpected running stats buffer "{name}" in state_dict for '
+                '{klass} with track_running_stats=False. If you are trying to '
+                'load a checkpoint saved before 0.4.0, this may be expected '
+                'because {klass} does not track running stats by default '
+                'anymore since 0.4.0. If running stats are not used, remove '
+                'them from state_dict before calling load_state_dict. '
+                'Otherwise, set track_running_stats=True in {klass} to use '
+                'running stats. See the documentation of {klass} for more '
+                'details.'
+                .format(name=full_name, klass=self.__class__.__name__))
+        super(_InstanceNorm, self)._load_state_dict_key_mismatch(
+            full_name, name, is_missing)
+
+    def forward(self, input):
+        self._check_input_dim(input)
+
+        return F.instance_norm(
+            input, self.running_mean, self.running_var, self.weight, self.bias,
+            self.training or not self.track_running_stats, self.momentum, self.eps)
+
+
+class InstanceNorm1d(_InstanceNorm):
+    r"""Applies Instance Normalization over a 2D or 3D input (a mini-batch of 1D
+    inputs with optional additional channel dimension) as described in the paper
+    `Instance Normalization: The Missing Ingredient for Fast Stylization`_ .
+
+    .. math::
+
+        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x]} + \epsilon} * \gamma + \beta
+
+    The mean and standard-deviation are calculated per-dimension separately
+    for each object in a mini-batch. :math:`\gamma` and :math:`\beta` are learnable parameter vectors
+    of size `C` (where `C` is the input size) if :attr:`affine` is ``True``.
+
+    By default, this layer uses instance statistics computed from input data in
+    both training and evaluation modes.
+
+    If :attr:`track_running_stats` is set to ``True``, during training this
+    layer keeps running estimates of its computed mean and variance, which are
+    then used for normalization during evaluation. The running estimates are
+    kept with a default :attr:`momentum` of 0.1.
+
+    .. note::
+        This :attr:`momentum` argument is different from one used in optimizer
+        classes and the conventional notion of momentum. Mathematically, the
+        update rule for running statistics here is
+        :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momemtum} \times x_t`,
+        where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
+        new observed value.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, L)` or :math:`L` from input of size :math:`(N, L)`
+        eps: a value added to the denominator for numerical stability. Default: 1e-5
+        momentum: the value used for the running_mean and running_var computation. Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``False``
+
+    Shape:
+        - Input: :math:`(N, C, L)`
+        - Output: :math:`(N, C, L)` (same shape as input)
+
+    Examples::
+
+        >>> # Without Learnable Parameters
+        >>> m = nn.InstanceNorm1d(100)
+        >>> # With Learnable Parameters
+        >>> m = nn.InstanceNorm1d(100, affine=True)
+        >>> input = torch.randn(20, 100, 40)
+        >>> output = m(input)
+
+    .. _`Instance Normalization: The Missing Ingredient for Fast Stylization`:
+        https://arxiv.org/abs/1607.08022
+    """
+
+    def _check_input_dim(self, input):
+        if input.dim() != 3:
+            raise ValueError('expected 3D input (got {}D input)'
+                             .format(input.dim()))
+
+
+class InstanceNorm2d(_InstanceNorm):
+    r"""Applies Instance Normalization over a 4D input (a mini-batch of 2D inputs
+    with additional channel dimension) as described in the paper
+    `Instance Normalization: The Missing Ingredient for Fast Stylization`_ .
+
+    .. math::
+
+        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x]} + \epsilon} * \gamma + \beta
+
+    The mean and standard-deviation are calculated per-dimension separately
+    for each object in a mini-batch. :math:`\gamma` and :math:`\beta` are learnable parameter vectors
+    of size `C` (where `C` is the input size) if :attr:`affine` is ``True``.
+
+    By default, this layer uses instance statistics computed from input data in
+    both training and evaluation modes.
+
+    If :attr:`track_running_stats` is set to ``True``, during training this
+    layer keeps running estimates of its computed mean and variance, which are
+    then used for normalization during evaluation. The running estimates are
+    kept with a default :attr:`momentum` of 0.1.
+
+    .. note::
+        This :attr:`momentum` argument is different from one used in optimizer
+        classes and the conventional notion of momentum. Mathematically, the
+        update rule for running statistics here is
+        :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momemtum} \times x_t`,
+        where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
+        new observed value.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, H, W)`
+        eps: a value added to the denominator for numerical stability. Default: 1e-5
+        momentum: the value used for the running_mean and running_var computation. Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``False``
+
+    Shape:
+        - Input: :math:`(N, C, H, W)`
+        - Output: :math:`(N, C, H, W)` (same shape as input)
+
+    Examples::
+
+        >>> # Without Learnable Parameters
+        >>> m = nn.InstanceNorm2d(100)
+        >>> # With Learnable Parameters
+        >>> m = nn.InstanceNorm2d(100, affine=True)
+        >>> input = torch.randn(20, 100, 35, 45)
+        >>> output = m(input)
+
+    .. _`Instance Normalization: The Missing Ingredient for Fast Stylization`:
+        https://arxiv.org/abs/1607.08022
+    """
+
+    def _check_input_dim(self, input):
+        if input.dim() != 4:
+            raise ValueError('expected 4D input (got {}D input)'
+                             .format(input.dim()))
+
+
+class InstanceNorm3d(_InstanceNorm):
+    r"""Applies Instance Normalization over a 5D input (a mini-batch of 3D inputs
+    with additional channel dimension) as described in the paper
+    `Instance Normalization: The Missing Ingredient for Fast Stylization`_ .
+
+    .. math::
+
+        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x]} + \epsilon} * \gamma + \beta
+
+    The mean and standard-deviation are calculated per-dimension separately
+    for each object in a mini-batch. :math:`\gamma` and :math:`\beta` are learnable parameter vectors
+    of size C (where C is the input size) if :attr:`affine` is ``True``.
+
+    By default, this layer uses instance statistics computed from input data in
+    both training and evaluation modes.
+
+    If :attr:`track_running_stats` is set to ``True``, during training this
+    layer keeps running estimates of its computed mean and variance, which are
+    then used for normalization during evaluation. The running estimates are
+    kept with a default :attr:`momentum` of 0.1.
+
+    .. note::
+        This :attr:`momentum` argument is different from one used in optimizer
+        classes and the conventional notion of momentum. Mathematically, the
+        update rule for running statistics here is
+        :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momemtum} \times x_t`,
+        where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
+        new observed value.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, D, H, W)`
+        eps: a value added to the denominator for numerical stability. Default: 1e-5
+        momentum: the value used for the running_mean and running_var computation. Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``False``
+
+    Shape:
+        - Input: :math:`(N, C, D, H, W)`
+        - Output: :math:`(N, C, D, H, W)` (same shape as input)
+
+    Examples::
+
+        >>> # Without Learnable Parameters
+        >>> m = nn.InstanceNorm3d(100)
+        >>> # With Learnable Parameters
+        >>> m = nn.InstanceNorm3d(100, affine=True)
+        >>> input = torch.randn(20, 100, 35, 45, 10)
+        >>> output = m(input)
+
+    .. _`Instance Normalization: The Missing Ingredient for Fast Stylization`:
+        https://arxiv.org/abs/1607.08022
+    """
+
+    def _check_input_dim(self, input):
+        if input.dim() != 5:
+            raise ValueError('expected 5D input (got {}D input)'
+                             .format(input.dim()))
