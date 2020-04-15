4f13cbc1976881dfc5056c160ccb7b17800c9efb
tryalice <48514380+tryalice@users.noreply.github.com>
2020-04-15 12:06:09 +0200

    Initialize driazati/testt

---
+++ test/test_jit.py
@@ -0,0 +1,10 @@
+# from newjit import *
+# import .newjit
+from newjit.test_jit import *
+import unittest
+# from common_utils import run_tests
+
+if __name__ == '__main__':
+    # suite = unittest.TestLoader().loadTestsFromModule(newjit.test_jit)
+    # unittest.TextTestRunner(verbosity=2).run(suite)
+    run_tests()
