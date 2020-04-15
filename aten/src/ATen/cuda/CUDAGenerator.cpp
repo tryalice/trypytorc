aa02322b572daa151099809b009b4791afefc4ba
tryalice <48514380+tryalice@users.noreply.github.com>
2020-04-15 14:11:32 +0200

    Initialize gh/bwasti/11/orig

---
+++ aten/src/ATen/cuda/CUDAGenerator.cpp
@@ -0,0 +1,71 @@
+#include <ATen/Config.h>
+
+#include <ATen/CUDAGenerator.h>
+#include <ATen/Context.h>
+#include <THC/THCTensorRandom.h>
+#include <stdexcept>
+
+// There is only one CUDAGenerator instance. Calls to seed(), manualSeed(),
+// initialSeed(), and unsafeGetTH() refer to the THCGenerator on the current
+// device.
+
+THCGenerator* THCRandom_getGenerator(THCState* state);
+
+namespace at {
+
+CUDAGenerator::CUDAGenerator(Context * context_)
+  : Generator(Device(DeviceType::CUDA)), context(context_)
+{
+}
+
+CUDAGenerator::~CUDAGenerator() {
+  // no-op Generator state is global to the program
+}
+
+CUDAGenerator& CUDAGenerator::copy(const Generator& from) {
+  throw std::runtime_error("CUDAGenerator::copy() not implemented");
+}
+
+CUDAGenerator& CUDAGenerator::free() {
+  THCRandom_shutdown(context->getTHCState());
+  return *this;
+}
+
+uint64_t CUDAGenerator::seed() {
+  return THCRandom_initialSeed(context->getTHCState());
+}
+
+uint64_t CUDAGenerator::current_seed() const {
+  return THCRandom_initialSeed(context->getTHCState());
+}
+
+void CUDAGenerator::set_current_seed(uint64_t seed) {
+  THCRandom_manualSeed(context->getTHCState(), seed);
+}
+
+/*
+ * Gets the DeviceType of CUDAGenerator.
+ * Used for type checking during run time.
+ */
+DeviceType CUDAGenerator::device_type() {
+  return DeviceType::CUDA;
+}
+
+CUDAGenerator& CUDAGenerator::manualSeedAll(uint64_t seed) {
+  THCRandom_manualSeedAll(context->getTHCState(), seed);
+  return *this;
+}
+
+void * CUDAGenerator::unsafeGetTH() {
+  return (void*)THCRandom_getGenerator(context->getTHCState());
+}
+
+std::shared_ptr<CUDAGenerator> CUDAGenerator::clone() const {
+  return std::shared_ptr<CUDAGenerator>(this->clone_impl());
+}
+
+CUDAGenerator* CUDAGenerator::clone_impl() const {
+  return new CUDAGenerator(context);
+}
+
+} // namespace at
